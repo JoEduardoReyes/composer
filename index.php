eaddc879e4acfadf17be96d1fb94c3819cadd2ba
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-01-04 11:29:46 -0600

    Introduccion a PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Comentario de
+varias
+lineas
+*/
+
+class answer
+{
+  /**
+   * Comenrtario de varias
+   * lineas para 
+   * palabra
+   */
+  protected $cliens = []; // comentarios de una sola linea
+
+  protected $insures = []; # comentarios de una sola linea
+
+}
