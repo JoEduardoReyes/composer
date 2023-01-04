926c81b447158fc6250605a0481970573637388d
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-01-04 11:53:26 -0600

    Sintaxis básica de PHP: comparación y variables

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+/* 
 // asignacion
 
 $num = 9;
@@ -15,3 +16,19 @@ echo "Resta de 2 - 2 es" . ((int) 2 - (int) 2);
 echo "Multi de 2 * 2 es" . ((int) 2 * (int) 2);
 echo "Divide de 2 / 2 es" . ((int) 2 / (int) 2);
 echo "Potencia de 2 ** 2 es" . ((int) 2 ** (int) 2);
+
+*/
+
+// comparacion
+// igual == 'valor'
+// igual === 'tipo y valor'
+// diferencia != 'valor'
+// diferencia !== 'tipo y valor'
+// valor numerico < > <= >=
+
+// variables variables y refrencias
+$app = 'name';
+$name = 'platzi';
+
+echo $app;
+echo $$app;
