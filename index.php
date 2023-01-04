1c49940bd22b5d4bc1b98ce4b0e45e31146bfe08
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-01-04 11:46:21 -0600

    Sintaxis básica de PHP: asignación y aritmética

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
 
-/* Comentario de
-varias
-lineas
-*/
+// asignacion
 
-class answer
-{
-  /**
-   * Comenrtario de varias
-   * lineas para 
-   * palabra
-   */
-  protected $cliens = []; // comentarios de una sola linea
+$num = 9;
+$lang = [
+  'es' => 'español',
+  'en' => 'ingles'
+];
 
-  protected $insures = []; # comentarios de una sola linea
+// aritmetica
 
-}
+echo "Suma de 2 + 2 es" . ((int) 2 + (int) 2);
+echo "Resta de 2 - 2 es" . ((int) 2 - (int) 2);
+echo "Multi de 2 * 2 es" . ((int) 2 * (int) 2);
+echo "Divide de 2 / 2 es" . ((int) 2 / (int) 2);
+echo "Potencia de 2 ** 2 es" . ((int) 2 ** (int) 2);
